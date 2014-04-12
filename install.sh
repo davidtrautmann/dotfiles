4792960ec9d3847aa8f5b71a3c8cc391cf7bb20a
David Trautmann <david.trautmann@gmail.com>
2014-04-12 18:23:15 +0200

    .gemrc dofile added

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".gitmodules .gvimrc .vimrc .vim .vimbackup .zlogin .zshrc .zsh"    # list of files/folders to symlink in homedir
+files=".gemrc .gitmodules .gvimrc .vimrc .vim .vimbackup .zlogin .zshrc .zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
